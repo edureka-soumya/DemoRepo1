e09972629942a95be041a1772a4e754095fee7c7
edureka-soumya <84872701+edureka-soumya@users.noreply.github.com>
2021-05-27 02:40:38 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Hello World");
+      System.out.println("Hello World123");
+   }
+}
